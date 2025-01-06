d42b85705f259a4ee8cfd51411b564ff890ed9f7
Saurabh <devsaurabh086@gmail.com>
2025-01-06 16:52:41 +0530

    new commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let themeButton = document.getElementsByClassName("theme-btn");
+let page = document.getElementsByClassName("page");
+let col = document.getElementsByClassName("col");
+console.log(page);
+console.log(themeButton);
+
 let currentPlayer = "X";
 
 let arr = Array(9).fill(null);
@@ -11,3 +17,35 @@ let handleClick = (e) => {
     e.innerText = currentPlayer;
 
 }
+// Loop through each theme button :
+for (let i = 0; i < themeButton.length; i++) {
+    themeButton[i].addEventListener("click", () => {
+        // Loop through each page element
+        for (let j = 0; j < page.length; j++) {
+            if (page[j].style.background === "white" || page[j].style.background === "") {
+                page[j].style.background = "#303030";
+                page[j].style.color = "white";
+                for (let i = 0; i < col.length; i++){
+                    col[i].style.color = "black";
+                }
+                for (let i = 0; themeButton.length; i++){
+                    themeButton[i].style.background = "yellow";
+                    themeButton[i].style.color = "red";
+                }
+               
+            } else {
+                page[j].style.background = "white";
+                page[j].style.color = "black";
+                for (let i = 0; themeButton.length; i++){
+                    themeButton[i].style.background = "#303030";
+                    themeButton[i].style.color = "white";
+                }
+                
+            }
+        }
+    });
+
+
+}
+
+
